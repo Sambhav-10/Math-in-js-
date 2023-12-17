4f98d4ff17a02b48c76c7289262783688f90cc79
Sambhav-10 <148258778+Sambhav-10@users.noreply.github.com>
2023-12-17 18:17:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,89 @@
+
+//         var a = 10;
+// var b= a+10;
+// console.log(b);
+
+
+
+// var x = function myFunction(a, b) {
+//   return a * b+3;
+// }
+
+// console.log(x(10,20))
+
+
+
+
+// var numAry = [];
+// for (var i = 0; i < 3; i++) {
+//   numAry.push(i);
+// }
+// console.log(numAry);
+// console.log(i);
+// numAry.pop();
+// console.log(numAry);
+
+
+
+// ............/..the function constuctor..... 
+// const myFunction = new Function("a", "b", "return a * b");
+
+// let x = myFunction(4, 5);
+
+// console.log(x)
+
+
+// ............this is self invoking function
+// (function () {
+//   document.write("Hello! I called myself");
+// })();
+
+
+// learn number and math.............
+
+// const num=400000
+
+// console.log(num.toFixed(1))// how mush digit you want to add in decimal
+
+// console.log(num.toPrecision(6))// how much digit you see--this value this 6 figer you can put in this function value 3-this function provide value is three but and remaing value in like -e10-2//
+
+
+// console.log(num.toPrecision(3))//this function provide how much digit you want them convert
+  
+// console.log(num.toLocaleString());//this value add coma to value is esay to readable -this funtion defult value in us standerd but we can change
+
+// ..............// math function librery
+
+// console.log(Math.abs(-4));//this function provide absolute value if your value in minus this converted in plus----and you think this is work in oposite--answer is no -this is only work in minus value
+
+
+
+// console.log(Math.round(4.5));//this function convert valu in round if value is range of =4.4 ans=4 but value= 4.5 ans=5
+
+// console.log(Math.ceil(4.1));// this function is ceil-ceiling-home ceiling -matlb top  this function always provide higher value 4.1=5
+
+// console.log(Math.floor(7.90));// this function always provide loweer value - 4.9 =4
+
+// console.log(Math.sqrt(100));//sqrt rot
+
+
+// console.log(Math.min(44,55,80,1,0))// this function provide minimum value 
+// console.log(Math.max(44,55,80,1,0))// this function provide miximum value 
+
+
+// console.log(Math.random()*100+1)
+
+// const min=10
+// const max=20
+// console.log(Math.floor(Math.random() * (max - min + 1 )) + min) ;
+
+
+// console.log() 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
